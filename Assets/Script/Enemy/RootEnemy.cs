67a869b3e9919d7cdc1ce34666ddb4f318731f33
Rose Meivogel <rose.meivogel@derolfgroep.nl>
2023-02-04 15:07:31 +0100

    Animation!

--- Assets/Script/Enemy/RootEnemy.cs
+++ Assets/Script/Enemy/RootEnemy.cs
@@ -1,3 +1,4 @@
+using Spine.Unity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,23 +20,43 @@ public class RootEnemy : MonoBehaviour
     [SerializeField]
     private SpriteRenderer spriteRenderer = null;
 
+    [SerializeField]
+    private Animator animator = null;
+
+    [SerializeField]
+    private SkeletonMecanim skeletonMecanim = null;
+
+    [SerializeField]
+    private AudioClip spawnInClip = null;
+
+
     private Collider2D playerCollider = null;
     public void SetUp(Collider2D playerCollider)
     {
         this.playerCollider = playerCollider;
-        spriteRenderer.color = Color.clear;
+
+        this.collider2D.enabled = false;
         Physics2D.IgnoreCollision(this.collider2D, playerCollider, true);
         StartCoroutine(EnableHitPlayer());
+        skeletonMecanim.gameObject.SetActive(false);
     }
 
     private IEnumerator EnableHitPlayer()
     {
-        while (spriteRenderer.color != Color.white)
+        Color start = spriteRenderer.color;
+        spriteRenderer.color = Color.clear;
+        while (spriteRenderer.color != start)
         {
-            spriteRenderer.color = Color.Lerp(spriteRenderer.color, Color.white, timeToShootOut * Time.deltaTime);
+            spriteRenderer.color = Color.Lerp(spriteRenderer.color, start, timeToShootOut * Time.deltaTime);
             yield return new WaitForEndOfFrame();
         }
-        spriteRenderer.color = Color.white;
+        spriteRenderer.color = start;
+        spriteRenderer.gameObject.SetActive(false);
+        skeletonMecanim.gameObject.SetActive(true);
+        animator.SetTrigger("spawn");
+        AudioSource.PlayClipAtPoint(spawnInClip, transform.position);
+
+        this.collider2D.enabled = true;
         //yield return new WaitForSeconds(timeToShootOut);
         Physics2D.IgnoreCollision(this.collider2D, playerCollider, false);
     }
@@ -47,7 +68,13 @@ public class RootEnemy : MonoBehaviour
         {
             health -= projectile.Damage;
             if (health <= 0)
-                Destroy(this.gameObject);
+            {
+                animator.SetTrigger("despawn");
+                this.collider2D.enabled = false;
+                Physics2D.IgnoreCollision(this.collider2D, playerCollider, false);
+                Destroy(this.gameObject, 1f);
+            }
+             
         }
         else
         {
